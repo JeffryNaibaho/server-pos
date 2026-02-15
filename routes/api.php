1e2abfc7a959b595e48c03680c010d0a648ca1f6
Jeffry Sanjaya Naibaho <jeffrynaibaho18@gmail.com>
2026-02-15 23:27:44 +0700

    Fix namespace case sensitivity and add test route

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,7 @@ use App\Http\Controllers\Api\TransactionController;
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::post('/checkout', [TransactionController::class, 'store']);
+// Route Tes Tanpa Controller
+Route::get('/halo', function () {
+    return response()->json(['status' => 'Masuk Pak Eko! Routing Aman!']);
+});
