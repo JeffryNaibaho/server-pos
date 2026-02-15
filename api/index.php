13297711763cc0fe0f390d7f6525ffa7e5d4f870
Jeffry Sanjaya Naibaho <jeffrynaibaho18@gmail.com>
2026-02-16 05:22:48 +0700

    Fix Laravel base path detection in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,18 +1,22 @@
 <?php
 
-// 1. Setup Folder /tmp (Wajib)
+// 1. Setup Folder /tmp (Wajib untuk Vercel)
 $tmpPath = '/tmp/storage/bootstrap/cache';
 if (!is_dir($tmpPath)) {
     mkdir($tmpPath, 0777, true);
 }
 
-// 2. JURUS PEMINDAHAN (Crucial!)
-// Kita beri tahu Laravel untuk menyimpan file cache paket & services di /tmp
-// Ini menimpa perilaku default yang mencoba nulis ke folder asli (Read-Only)
+// 2. Cache Fix (Paket & Services)
 putenv('APP_PACKAGES_CACHE=' . $tmpPath . '/packages.php');
 putenv('APP_SERVICES_CACHE=' . $tmpPath . '/services.php');
 putenv('APP_ROUTES_CACHE=' . $tmpPath . '/routes-v7.php');
 putenv('APP_EVENTS_CACHE=' . $tmpPath . '/events.php');
 
-// 3. Masuk ke Laravel
+// 3. --- JURUS ANTI-POTONG (PENTING!) ---
+// Kita tipu Laravel: "Kamu jalan di /index.php, bukan /api/index.php"
+// Dengan begini, Laravel TIDAK AKAN membuang kata '/api' dari alamat URL.
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+
+// 4. Masuk ke Laravel
 require __DIR__ . '/../public/index.php';
